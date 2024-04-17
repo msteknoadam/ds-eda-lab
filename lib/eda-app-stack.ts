f5b5804458cbeb2e626a5aff4ca80152d5e931c0
msteknoadam <40995274+msteknoadam@users.noreply.github.com>
2024-04-18 00:22:19 +0100

    Signup resource

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -13,19 +13,47 @@ import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class EDAAppStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
 
-    const imagesBucket = new s3.Bucket(this, "images", {
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-      publicReadAccess: false,
-    });
+		const imagesBucket = new s3.Bucket(this, "images", {
+			removalPolicy: cdk.RemovalPolicy.DESTROY,
+			autoDeleteObjects: true,
+			publicReadAccess: false,
+		}); // Integration infrastructure
 
-    // Output
-    
-    new cdk.CfnOutput(this, "bucketName", {
-      value: imagesBucket.bucketName,
-    });
-  }
+		const queue = new sqs.Queue(this, "img-created-queue", {
+			receiveMessageWaitTime: cdk.Duration.seconds(10),
+		});
+
+		// Lambda functions
+
+		const processImageFn = new lambdanode.NodejsFunction(this, "ProcessImageFn", {
+			runtime: lambda.Runtime.NODEJS_18_X,
+			entry: `${__dirname}/../lambdas/processImage.ts`,
+			timeout: cdk.Duration.seconds(15),
+			memorySize: 128,
+		});
+
+		// S3 --> SQS
+		imagesBucket.addEventNotification(s3.EventType.OBJECT_CREATED, new s3n.SqsDestination(queue));
+
+		// SQS --> Lambda
+		const newImageEventSource = new events.SqsEventSource(queue, {
+			batchSize: 5,
+			maxBatchingWindow: cdk.Duration.seconds(10),
+		});
+
+		processImageFn.addEventSource(newImageEventSource);
+
+		// Permissions
+
+		imagesBucket.grantRead(processImageFn);
+
+		// Output
+
+		new cdk.CfnOutput(this, "bucketName", {
+			value: imagesBucket.bucketName,
+		});
+	}
 }
